e317e50d6d65e2cee83eaa8f062a80b1fbc0cf75
aemgeeks1212 <aemgeeks1212@gmail.com>
2021-01-03 00:35:40 +0530

    Tutorial #31 OSGi Configuration

---
+++ core/src/main/java/com/aem/geeks/core/models/OSGiConfigDemo.java
@@ -0,0 +1,9 @@
+package com.aem.geeks.core.models;
+
+public interface OSGiConfigDemo {
+    public String getServiceName();
+    public int getServiceCount();
+    public boolean isLiveData();
+    public String[] getCountries() ;
+    public String getRunModes();
+}
---
+++ core/src/main/java/com/aem/geeks/core/models/impl/OSGiConfigDemoImpl.java
@@ -0,0 +1,43 @@
+package com.aem.geeks.core.models.impl;
+
+import com.aem.geeks.core.models.OSGiConfigDemo;
+import com.aem.geeks.core.models.ServiceDemo;
+import com.aem.geeks.core.services.OSGiConfig;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+
+@Model(adaptables = SlingHttpServletRequest.class,
+        adapters = OSGiConfigDemo.class,
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class OSGiConfigDemoImpl implements OSGiConfigDemo{
+
+    @OSGiService
+    OSGiConfig oSGiConfig;
+
+    @Override
+    public String getServiceName() {
+        return oSGiConfig.getServiceName();
+    }
+
+    @Override
+    public int getServiceCount() {
+        return oSGiConfig.getServiceCount();
+    }
+
+    @Override
+    public boolean isLiveData() {
+        return oSGiConfig.isLiveData();
+    }
+
+    @Override
+    public String[] getCountries() {
+        return oSGiConfig.getCountries();
+    }
+
+    @Override
+    public String getRunModes() {
+        return oSGiConfig.getRunModes();
+    }
+}
